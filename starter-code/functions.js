d7f4155f7e46360f18acb94dfb97b1c9751b819b
Hristina Lapanova <hristina.lapanova@gmail.com>
2019-09-19 14:15:22 -0400

    first commit

---
+++ starter-code/functions.js
@@ -0,0 +1,80 @@
+// Question 1
+function maxOfTwoNumbers(a, b) {
+if (a> b){
+    return a
+}
+else if (a <b){
+    return b
+}
+}
+function maxOfTwoNumbers(a, b) {
+    return (isBigger ? a : b);
+  }
+
+// Question 2
+function maxOfThree(a, b, c) {
+if (a > b && a > c){
+    return a
+}
+else if (b > a && b > c){
+    return b
+}
+else if (c > a && c > b){
+    return c
+}
+}
+
+// Question 3
+function isCharacterAVowel(char) {
+if ( char === a|| char === e || char === o || char === u || char === i
+    ){
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+
+// Question 4: Part 1
+
+function sumArray(arr) {
+    let sum=0;
+for(let i=0; i<arr.length; i++){
+sum=arr[i]+sum
+}
+}
+
+// Question 4: Part 2
+
+function multiplyArray(arr) {
+    let sum=1;
+for(let i=0; i<arr.length; i++){
+    sum=sum*arr[i]
+}
+}
+
+// Question 5
+const numberOfArguments = function() {
+
+}
+
+// Question 6
+const reverseString = function() {
+
+}
+
+// Question 7
+function findLongestWord() {
+
+}
+
+// Question 8
+function filterLongWords() {
+
+}
+
+// Bonus 1
+function charactersOccurencesCount() {
+
+}
